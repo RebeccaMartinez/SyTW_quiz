3610ca26d696068d7683f75a7d2e17a83d86452f
Rebecca Martinez Galan <rebe.mart.gal@gmail.com>
2015-11-19 20:25:52 +0000

    creada la clase respuesta

---
+++ models/respuesta.js
@@ -0,0 +1,16 @@
+function Respuesta(res){
+  this.res = res;
+  if(typeof(this.res) === 'string' || typeof(this.res) === 'number') {
+    return function(x){return x === this.res;};  
+  }
+  else if (this.res instanceof RegExp) {
+    return function(x){return (this.res).exec(x);};
+  }
+  else {
+    return function(x){
+      
+    };
+  }
+}
+
+module.exports = Respuesta;
