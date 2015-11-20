b5a7f85046b71ea4622ba68167dc896a6d78f05c
Rebecca Martinez Galan <rebe.mart.gal@gmail.com>
2015-11-20 11:04:10 +0000

    simplificada la clase pregunta

--- models/pregunta.js
+++ models/pregunta.js
@@ -3,38 +3,5 @@ function Pregunta(preg){
   this.tipo = tipo;
 }
 
-function PreguntaCorta(preg){
-  Pregunta.call(this, preg);
-  tipo = "corta";
-}
-
-PreguntaCorta.prototype.getTipo = function(){
-  return this.tipo;
-}
-
-PreguntaCorta.prototype.getPreg = function(){
-  return this.preg;
-}
-
-function PreguntaLarga(preg){
-  Pregunta.call(this, preg);
-  tipo = "larga";
-}
-
-PreguntaLarga.prototype.getTipo = function(){
-  return this.tipo;
-}
-
-PreguntaLarga.prototype.getPreg = function(){
-  return this.preg;
-}
-
-PreguntaCorta.prototype = new Pregunta();
-PreguntaCorta.prototype.constructor = PreguntaCorta;
-PreguntaLarga.prototype = new Pregunta();
-PreguntaLarga.prototype.constructor = PreguntaLarga;
-
-
 module.exports = Pregunta;
-module.exports = PreguntaCorta;
-module.exports = PreguntaLarga;
+
