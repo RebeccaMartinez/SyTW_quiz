2a652b52246ca2bb15f1d00082de19aadfb9256f
Rebecca Martinez Galan <rebe.mart.gal@gmail.com>
2015-11-27 08:53:51 +0000

    Camiando cosas

--- models/PSeleccionSimple.js
+++ models/PSeleccionSimple.js
@@ -3,11 +3,12 @@ var Pregunta = require('../models/pregunta.js');
 function PSeleccionSimple(preg, opciones){
   Pregunta.call(this, preg);
   this.opciones = opciones;
-  var aux = [];
+  this.vista = "<input type='text' name='respuesta' placeholder='Responda aquí' autofocus=''>";
+  /*var aux = [];
   for(var i=0; i<this.opciones.length; i++){
     aux[i] = "<input type='radio' name='respuesta' + value=" + this.opciones[i] + "</option>";
   }
-  this.vista = aux;
+  this.vista = aux;*/
 }
 
 PSeleccionSimple.prototype = new Pregunta();
--- models/preguntaCorta.js
+++ models/preguntaCorta.js
@@ -3,6 +3,7 @@ var Pregunta = require('../models/pregunta.js');
 function PreguntaCorta(preg){
   Pregunta.call(this, preg);
   this.vista = "<input type='text' name='respuesta' placeholder='Responda aquí' autofocus=''>";
+
 }
 
 
